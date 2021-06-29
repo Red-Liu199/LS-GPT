a14e49c3c10c7e57ff9b58e8241b86d8c3ad1dd7
Red-Liu199 <58842528+Red-Liu199@users.noreply.github.com>
2021-06-29 11:02:35 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 # Copyright 2021 Tsinghua SPMI Lab, Author: Hong Liu
 # This script run the supervised baseline with 100% labeled data.
+posterior=False
 python train_semi.py -mode train\
     -cfg  lr=1e-4\
     gradient_accumulation_steps=16 batch_size=2\
@@ -8,4 +9,5 @@ python train_semi.py -mode train\
     model_act=True\
     dataset=1\
     delex_as_damd=True\
-    gen_db=False
+    gen_db=False\
+    posterior_train=$posterior
